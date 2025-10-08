676c85f837d1609434d2200a8292bd83f5a4ee48
MiguelAndersonCunha <160076154+MiguelAndersonCunha@users.noreply.github.com>
2025-10-07 21:37:53 -0300

    Criação do HTML, CSS e JS

---
+++ iPortSolutions/iPortSolutions/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿const botao_adicionar = document.getElementById('adicionar');
+const formulario = document.querySelector('.form');
+formulario.classList.add('escondido');
+
+botao_adicionar.addEventListener('click', function(e) {
+    e.preventDefault();
+    formulario.classList.remove('escondido');
+});
+
+const codigo = document.getElementById('codigo');
+const data_entrada = document.getElementById('data_entrada');
+const data_saida = document.getElementById('data_saida');
+
+const botao_cadastrar = document.getElementById('cadastrar');
+botao_cadastrar.addEventListener('click', function (e) {
+    e.preventDefault();
+});
